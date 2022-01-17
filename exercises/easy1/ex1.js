57685d75b8d04077e0f3d03043218367f88d7593
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-17 23:19:18 +0100

    solved 1

---
+++ exercises/easy1/ex1.js
@@ -0,0 +1,8 @@
+const isOdd = num => !!(num % 2);
+
+console.log(isOdd(2)); // => false
+console.log(isOdd(5)); // => true
+console.log(isOdd(-17)); // => true
+console.log(isOdd(-8)); // => false
+console.log(isOdd(0)); // => false
+console.log(isOdd(7)); // => true
