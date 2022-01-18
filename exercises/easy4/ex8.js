2fab5fb88d7c27dc3cc1d7a1facc7153a34c6787
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-18 23:08:38 +0100

    solv ex8

---
+++ exercises/easy4/ex8.js
@@ -0,0 +1,16 @@
+const wordSizes = sentence => {
+  if (sentence.trim() === '') return {};
+  const sentenceArr = sentence.split(' ').map(word => word.length).sort();
+  const wordLengthObj = {};
+  for (let i = 0; i < sentenceArr.length; i += 1) {
+    if (wordLengthObj[sentenceArr[i]]) wordLengthObj[sentenceArr[i]] += 1;
+    else wordLengthObj[sentenceArr[i]] = 1;
+  }
+  console.log(wordLengthObj);
+  return wordLengthObj;
+};
+
+wordSizes('Four score and seven.');                       // { "3": 1, "4": 1, "5": 1, "6": 1 }
+wordSizes('Hey diddle diddle, the cat and the fiddle!');  // { "3": 5, "6": 1, "7": 2 }
+wordSizes("What's up doc?");                              // { "2": 1, "4": 1, "6": 1 }
+wordSizes('');
