f0e1148960b53159a3c3e0675e1a5c80595e9e68
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-18 11:44:55 +0100

    added more pseudocode

---
+++ lesson2/pseudocode.js
@@ -0,0 +1,11 @@
+// Func which returns two numbers together
+// Informal
+//  Get the two numbers
+//  Add them
+// Formal
+//  START
+//  SET num1 = first number
+//  SET num2 = second number
+//  SET sum = num1 + num2
+//  PRINT sum
+//  END
