98580d82c1010b271cd6d94c5d06f1e61ebb8411
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-18 13:04:34 +0100

    added config file

---
+++ lesson2/calcConfig.js
@@ -0,0 +1,10 @@
+const calcConfig = {
+  welcomeMsg: 'Welcome to the Calculator!',
+  firstNumPrompt: 'What is the first number?',
+  secondNumPrompt: 'What is the second number?',
+  operationPrompt: 'What is the operation?\n1) Add 2) Subtract 3) Multiply 4) Divide',
+  invalidOperationMsg: 'Invalid operation selected',
+  continuePrompt: 'Type "y" to perform another calculation: ',
+};
+
+module.exports = calcConfig;
