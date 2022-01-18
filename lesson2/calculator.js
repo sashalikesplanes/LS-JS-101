f8739c5568c36c20e1e2cd831fe312ff7c1fc9d0
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-18 13:04:48 +0100

    implemented getting messages from config file

--- lesson2/calculator.js
+++ lesson2/calculator.js
@@ -5,53 +5,59 @@
 // Display results of operation
 
 const readline = require('readline-sync');
+const calcConfig = require('./calcConfig');
 
 const prompt = msg => {
   console.log(`=> ${msg}`);
 };
 
-prompt('Welcome to the Calculator!');
+prompt(calcConfig.welcomeMsg);
 
 const invalidNumber = num => num.trim() === '' || Number.isNaN(Number(num));
 
-let number1;
+let continueCalc;
 do {
-  prompt('What is the first number?');
-  number1 = readline.question();
-} while (invalidNumber(number1));
+  let number1;
+  do {
+    prompt(calcConfig.firstNumPrompt);
+    number1 = readline.question();
+  } while (invalidNumber(number1));
 
-let number2;
-do {
-  prompt('What is the second number?');
-  number2 = readline.question();
-} while (invalidNumber(number2));
+  let number2;
+  do {
+    prompt(calcConfig.secondNumPrompt);
+    number2 = readline.question();
+  } while (invalidNumber(number2));
 
-let operation;
-do {
-  prompt('What is the operation?\n1) Add 2) Subtract 3) Multiply 4) Divide');
-  operation = readline.question();
-} while (!['1', '2', '3', '4'].includes(operation));
+  let operation;
+  do {
+    prompt(calcConfig.operationPrompt);
+    operation = readline.question();
+  } while (!['1', '2', '3', '4'].includes(operation));
 
 
-number1 = Number(number1);
-number2 = Number(number2);
+  number1 = Number(number1);
+  number2 = Number(number2);
 
-let output;
-switch (operation) {
-  case '1':
-    output = number1 + number2;
-    break;
-  case '2':
-    output = number1 - number2;
-    break;
-  case '3':
-    output = number1 * number2;
-    break;
-  case '4':
-    output = number1 / number2;
-    break;
-  default:
-    console.log('Invalid operation selected');
-}
+  let output;
+  switch (operation) {
+    case '1':
+      output = number1 + number2;
+      break;
+    case '2':
+      output = number1 - number2;
+      break;
+    case '3':
+      output = number1 * number2;
+      break;
+    case '4':
+      output = number1 / number2;
+      break;
+    default:
+      console.log(calcConfig.invalidOperationMsg);
+  }
 
-if (output) console.log(`The result is ${output}`);
+  if (output) console.log(`The result is ${output}`);
+  prompt(calcConfig.continuePrompt);
+  continueCalc = readline.question();
+} while (continueCalc === 'y');
