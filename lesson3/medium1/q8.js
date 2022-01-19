0b95e10010cfb163c73bea2907624834a2de576b
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-19 15:06:24 +0100

    solved 7 and 8, also 9 and 10 in head

---
+++ lesson3/medium1/q8.js
@@ -0,0 +1,20 @@
+let munsters = {
+  Herman: { age: 32, gender: "male" },
+  Lily: { age: 30, gender: "female" },
+  Grandpa: { age: 402, gender: "male" },
+  Eddie: { age: 10, gender: "male" },
+  Marilyn: { age: 23, gender: "female" }
+};
+
+function messWithDemographics(demoObject) {
+  Object.values(demoObject).forEach(familyMember => {
+    familyMember["age"] += 42;
+    familyMember["gender"] = "other";
+  });
+}
+
+messWithDemographics(munsters);
+console.log(munsters);
+// Yes it did get ransacked as the values of the munsters object are objects themselves
+// Hence in the function they are passed by reference, hence the actual family member
+// objects in memory are modified
