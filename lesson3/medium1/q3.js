c7a43cd88e3cd2ec3e559962138a453f2b50c30a
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-19 14:58:45 +0100

    solv 3 and 4

---
+++ lesson3/medium1/q3.js
@@ -0,0 +1,18 @@
+
+function factors(number) {
+  let divisor = number;
+  let factors = [];
+  while (divisor > 0) {
+    if (number % divisor === 0) {
+      factors.push(divisor);
+    }
+    divisor -= 1;
+  }
+  return factors;
+}
+
+console.log(factors(5));
+console.log(factors(6));
+console.log(factors(10));
+console.log(factors(0));
+console.log(factors(-10));
---
+++ lesson3/medium1/q4.js
@@ -0,0 +1,17 @@
+function addToRollingBuffer1(buffer, maxBufferSize, newElement) {
+  buffer.push(newElement);
+  if (buffer.length > maxBufferSize) {
+    buffer.shift();
+  }
+  return buffer;
+}
+
+function addToRollingBuffer2(buffer, maxBufferSize, newElement) {
+  buffer = buffer.concat(newElement);
+  if (buffer.length > maxBufferSize) {
+    buffer.shift();
+  }
+  return buffer;
+}
+
+// Yes there is difference, implementation 1 is destrictuve to the buffer passed
