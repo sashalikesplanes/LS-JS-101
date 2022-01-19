f0e2a1ceef2ad5bfee58470241bb9ed672a8fecf
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-19 12:55:21 +0100

    blank files

--- lesson3/easy1/q9.js
+++ lesson3/easy1/q9.js
@@ -4,9 +4,3 @@ flintstones.push("Dino", "Hoppy");
 console.log(flintstones);
 
 
-let advice = "Few things in life are as important as house training your pet dinosaur.";
-
-// Expected return value:
-// => 'Few things in life are as important as '
-advice = advice.match(/(.*?)house/g);
-console.log(advice);
