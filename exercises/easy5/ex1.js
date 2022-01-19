76341af85319507b4bd5c8b9c153f53e93f13fcf
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-19 17:07:52 +0100

    added further exploration to 1

--- exercises/easy5/ex1.js
+++ exercises/easy5/ex1.js
@@ -2,12 +2,18 @@ const dms = angleDegresWithDecimal => {
   const MINUTES_PER_DEGREE = 60;
   const SECONDS_PER_DEGREE = 60 * 60;
 
-  const angleDegrees = Math.floor(angleDegresWithDecimal);
+  let angleDegrees = Math.floor(angleDegresWithDecimal);
   const angleMinutes = Math.floor((angleDegresWithDecimal - angleDegrees)
                                         * MINUTES_PER_DEGREE);
   const angleSeconds = Math.floor((angleDegresWithDecimal - angleDegrees
                                   - (angleMinutes / MINUTES_PER_DEGREE))
                                   * SECONDS_PER_DEGREE);
+
+  while (angleDegrees < 0 || angleDegrees > 360) {
+    if (angleDegrees < 0) angleDegrees += 360;
+    else if (angleDegrees > 360) angleDegrees -= 360;
+  }
+
   console.log(`${angleDegrees}°${String(angleMinutes).padStart(2, '0')}'`
               + `${String(angleSeconds).padStart(2, '0')}"`);
 };
@@ -18,3 +24,8 @@ dms(254.6);        // 254°35'59"
 dms(93.034773);    // 93°02'05"
 dms(0);            // 0°00'00"
 dms(360);          // 360°00'00" or 0°00'00"
+
+dms(-1);   // 359°00'00"
+dms(400);  // 40°00'00"
+dms(-40);  // 320°00'00"
+dms(-420); // 300°00'00"
