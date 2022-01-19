893943f93fc471cd6d118698696772f3086511f6
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-20 00:23:54 +0100

    solv 9

---
+++ exercises/easy6/ex9.js
@@ -0,0 +1,5 @@
+const reverseSentence = string => string.split(' ').reverse().join(' ');
+
+console.log(reverseSentence(''));
+console.log(reverseSentence('Hello World'));
+console.log(reverseSentence('Reverse these words'));
