3be0786e01494bcead2e009335d36906b0557d11
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-19 18:38:34 +0100

    solved 5 with map

--- exercises/easy5/ex5.js
+++ exercises/easy5/ex5.js
@@ -5,3 +5,10 @@ const interleave = (arr1, arr2) => {
 };
 
 console.log(interleave([1, 2, 3], ['a', 'b', 'c']));
+
+const interleaveMap = (arr1, arr2) => {
+  const outArr = arr1.map((_, idx) => [arr1[idx], arr2[idx]]).flat();
+  return outArr;
+};
+
+console.log(interleaveMap([1, 2, 3], ['a', 'b', 'c']));
