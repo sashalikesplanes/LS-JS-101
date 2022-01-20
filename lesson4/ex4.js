4cf2b754ffb38cda8b24632e99f6c31c0c9050bf
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-20 01:50:36 +0100

    solv code ex4

---
+++ lesson4/ex4.js
@@ -0,0 +1,7 @@
+const multiply = (array, multiplier) => {
+  return array.map(value => value * multiplier);
+};
+
+let myNumbers = [1, 4, 3, 7, 2, 6];
+console.log(multiply(myNumbers, 3)); // => [3, 12, 9, 21, 6, 18]
+console.log(myNumbers);
