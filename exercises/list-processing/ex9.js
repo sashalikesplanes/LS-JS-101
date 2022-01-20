f96bae90015480fa9ae9ebacab0321da601788c6
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-20 22:09:46 +0100

    solv 9

---
+++ exercises/list-processing/ex9.js
@@ -0,0 +1,23 @@
+let transactions = [
+  { id: 101, movement: "in", quantity: 5 },
+  { id: 105, movement: "in", quantity: 10 },
+  { id: 102, movement: "out", quantity: 17 },
+  { id: 101, movement: "in", quantity: 12 },
+  { id: 103, movement: "out", quantity: 20 },
+  { id: 102, movement: "out", quantity: 15 },
+  { id: 105, movement: "in", quantity: 25 },
+  { id: 101, movement: "out", quantity: 18 },
+  { id: 102, movement: "in", quantity: 22 },
+  { id: 103, movement: "out", quantity: 15 },
+];
+
+// input array
+// output array
+// loop over all elements of transaction
+// add all elements with matching id to results
+
+const transactionsFor = (id, txList) => {
+  return txList.filter((tx) => tx.id === id);
+};
+
+console.log(transactionsFor(102, transactions));
