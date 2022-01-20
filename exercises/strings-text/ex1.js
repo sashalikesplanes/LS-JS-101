ef501ac1287e5b01d81704c839197085c8dfe194
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-20 22:20:29 +0100

    solv and refactt ex1

---
+++ exercises/strings-text/ex1.js
@@ -0,0 +1,10 @@
+const isUppercase = (string) => {
+  return string === string.toUpperCase();
+};
+
+console.log(isUppercase("t")); // false
+console.log(isUppercase("T")); // true
+console.log(isUppercase("Four Score")); // false
+console.log(isUppercase("FOUR SCORE")); // true
+console.log(isUppercase("4SCORE!")); // true
+console.log(isUppercase("")); // true
