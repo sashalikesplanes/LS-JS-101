73393043821f8e8d0539302a4e2ecd00a621e0d9
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-21 04:14:29 +0100

    solv 5

---
+++ lesson5/p5.js
@@ -0,0 +1,13 @@
+let munsters = {
+  Herman: { age: 32, gender: "male" },
+  Lily: { age: 30, gender: "female" },
+  Grandpa: { age: 402, gender: "male" },
+  Eddie: { age: 10, gender: "male" },
+  Marilyn: { age: 23, gender: "female" },
+};
+
+let totalAge = Object.values(munsters).reduce(
+  (sum, munster) => sum + munster.age,
+  0
+);
+console.log(totalAge);
